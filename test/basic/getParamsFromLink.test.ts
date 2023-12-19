ac3aac91068cb42c68991b9753e86acb1cf7d9b1
Hugo Montenegro <h@hugomontenegro.com>
2023-12-19 10:04:33 +0000

    chore: add test case

--- test/basic/getParamsFromLink.test.ts
+++ test/basic/getParamsFromLink.test.ts
@@ -39,4 +39,16 @@ describe('getParamsFromLink', function () {
 			trackId: 'ui',
 		})
 	}, 10000)
+
+	it('no tag', async function () {
+		const params = peanut.getParamsFromLink('http://peanut.to/claim?c=5&v=v4&i=4663#p=ULKr1Bzoh1SInyLO')
+
+		expect(params).toEqual({
+			chainId: 5,
+			contractVersion: 'v4',
+			depositIdx: 4663,
+			password: 'ULKr1Bzoh1SInyLO',
+			trackId: '',
+		})
+	}, 10000)
 })
