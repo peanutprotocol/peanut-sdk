29798c59a7dd93fba3cf2ab042581df441de77d3
borcherd <borcherd@me.com>
2024-01-30 14:25:48 +0400

    feat: fixes to create and get multilink functions

--- test/basic/getLinksFromMultilink.test.ts
+++ test/basic/getLinksFromMultilink.test.ts
@@ -14,6 +14,18 @@ describe('getLinksFromMultilink function', () => {
 		expect(actualLinks).toEqual(expectedLinks)
 	})
 
+	it('should correctly split a multilink into individual links with hash before password', () => {
+		const multilink = 'https://peanut.to/claim?c=1,2,3&v=v3&i=101,102,103#p=ggHmiXLTAG6Kwjvr'
+		const expectedLinks = [
+			'https://peanut.to/claim?c=1&v=v3&i=101#p=ggHmiXLTAG6Kwjvr',
+			'https://peanut.to/claim?c=2&v=v3&i=102#p=ggHmiXLTAG6Kwjvr',
+			'https://peanut.to/claim?c=3&v=v3&i=103#p=ggHmiXLTAG6Kwjvr',
+		]
+
+		const actualLinks = peanut.getLinksFromMultilink(multilink)
+		expect(actualLinks).toEqual(expectedLinks)
+	})
+
 	it('should handle a single c parameter correctly', () => {
 		const multilink = 'https://peanut.to/claim#?c=1&v=v3&i=101,102,103&p=ggHmiXLTAG6Kwjvr'
 		const expectedLinks = [
