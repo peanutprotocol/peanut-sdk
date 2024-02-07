dd0dcbe3978f1ab91e8b3cd2dcbf4cf306662b86
Hugo Montenegro <h@hugomontenegro.com>
2024-02-07 14:18:08 +0000

    feat: publish

--- test/basic/getAllUnclaimedDepositsWithIdxForAddress.test.ts
+++ test/basic/getAllUnclaimedDepositsWithIdxForAddress.test.ts
@@ -7,7 +7,7 @@ dotenv.config()
 describe('getAllCreatedLinksForAddress', () => {
 	const ADDRESS = '0x2d826aD1EAD5c8a2bC46ab93d9D0c6BEe0d39918'
 
-	it('should return all deposits for the given address if the provider is undefined', async () => {
+	it('should return all deposits for the given address if the provider is undefined v4', async () => {
 		const chainId = '137'
 		const address = ADDRESS
 		const peanutContractVersion = 'v4'
@@ -23,10 +23,26 @@ describe('getAllCreatedLinksForAddress', () => {
 		expect(allCreatedLinks.length).toBeGreaterThan(0)
 	}, 150000)
 
-	it('should return all deposits for the given address if the provider is undefined', async () => {
+	it('should return all deposits for the given address if the provider is undefined v4.2', async () => {
 		const chainId = '137'
 		const address = ADDRESS
 		const peanutContractVersion = 'v4.2'
+
+		peanut.toggleVerbose(true)
+
+		const allCreatedLinks = await peanut.getAllUnclaimedDepositsWithIdxForAddress({
+			address,
+			chainId,
+			peanutContractVersion,
+		})
+
+		expect(allCreatedLinks.length).toBeGreaterThan(0)
+	}, 150000)
+
+	it('should return all deposits for the given address if the provider is undefined v4.3', async () => {
+		const chainId = '5000'
+		const address = '0x6B3751c5b04Aa818EA90115AA06a4D9A36A16f02'
+		const peanutContractVersion = 'v4.3'
 
 		peanut.toggleVerbose(true)
 
