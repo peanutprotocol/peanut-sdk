59c461836e18d545bd6a5d51b5bd07409d9e3852
Juan José Ramírez <artjjrn@gmail.com>
2024-10-14 13:31:08 -0300

    test: organize request tests

--- test/basic/RequestLinkXChain.test.ts
+++ test/basic/RequestLinkXChain.test.ts
@@ -7,72 +7,122 @@ import { EPeanutLinkType } from '../../src/consts/interfaces.consts'
 dotenv.config()
 
 describe('Peanut XChain request links fulfillment tests', function () {
-	it('USDC on Optimism to USDT on Polygon', async function () {
-		peanut.toggleVerbose(true)
-		const userPrivateKey = process.env.TEST_WALLET_PRIVATE_KEY!
+	it.each([
+		{
+			amount: '0.1',
+			sourceToken: {
+				chain: '10',
+				address: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85', // USDC on Optimism
+				decimals: 6,
+				name: 'USDC on Optimism',
+				type: EPeanutLinkType.erc20,
+			},
+			destinationToken: {
+				chain: '137',
+				address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F', // USDT on Polygon
+				decimals: 6,
+				name: 'USDT on Polygon',
+				type: EPeanutLinkType.erc20,
+			},
+		},
+		{
+			amount: '0.0001',
+			sourceToken: {
+				chain: '10',
+				address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', // ETH on Optimism
+				decimals: 18,
+				name: 'ETH on Optimism',
+				type: EPeanutLinkType.native,
+			},
+			destinationToken: {
+				chain: '42161',
+				address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', // ETH on Arbitrum
+				decimals: 18,
+				name: 'ETH on Arbitrum',
+				type: EPeanutLinkType.native,
+			},
+		},
+		{
+			amount: '0.1',
+			sourceToken: {
+				chain: '10',
+				address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', // ETH on Optimism
+				decimals: 18,
+				name: 'ETH on Optimism',
+				type: EPeanutLinkType.native,
+			},
+			destinationToken: {
+				chain: '10',
+				address: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85', // USDC on Optimism
+				decimals: 6,
+				name: 'USDC on Optimism',
+				type: EPeanutLinkType.erc20,
+			},
+		},
+	])(
+		'$sourceToken.name to $destinationToken.name',
+		async ({ amount, sourceToken, destinationToken }) => {
+			peanut.toggleVerbose(true)
+			const userPrivateKey = process.env.TEST_WALLET_X_CHAIN_USER!
 
-		// Parameters that affect the test behaviour
-		const sourceChainId = '10' // Optimism
-		const destinationChainId = '137' // Polygon
-		const amountToTestWith = 0.1
-		const tokenDecimals = 6
-		const apiUrl = process.env.PEANUT_API_URL!
-		const APIKey = process.env.PEANUT_DEV_API_KEY!
-		const sourceChainProvider = await getDefaultProvider(sourceChainId)
-		console.log('Source chain provider', sourceChainProvider)
+			// Parameters that affect the test behaviour
+			const apiUrl = process.env.PEANUT_API_URL!
+			const APIKey = process.env.PEANUT_DEV_API_KEY!
+			const sourceChainProvider = await getDefaultProvider(sourceToken.chain)
+			console.log('Source chain provider', sourceChainProvider)
 
-		const userSourceChainWallet = new ethers.Wallet(userPrivateKey, sourceChainProvider)
+			const userSourceChainWallet = new ethers.Wallet(userPrivateKey, sourceChainProvider)
 
-		const recipientAddress = new ethers.Wallet(process.env.TEST_WALLET_PRIVATE_KEY2!).address
-		const tokenAddress = '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85' // USDC on Optimism
-		const destinationToken = '0xc2132D05D31c914a87C6611C10748AEb04B58e8F' // USDT on Polygon
-		const { link } = await peanut.createRequestLink({
-			chainId: destinationChainId,
-			tokenAddress: destinationToken,
-			tokenAmount: amountToTestWith.toString(),
-			tokenType: EPeanutLinkType.erc20,
-			tokenDecimals: tokenDecimals.toString(),
-			recipientAddress,
-			APIKey,
-			apiUrl,
-		})
-		console.log('Created a request link on the source chain!', link)
+			const recipientAddress = new ethers.Wallet(process.env.TEST_WALLET_PRIVATE_KEY2!).address
 
-		const linkDetails = await peanut.getRequestLinkDetails({
-			link,
-			APIKey,
-			apiUrl,
-		})
-		console.log('Got the link details!', linkDetails)
-
-		const xchainUnsignedTxs = await peanut.prepareXchainRequestFulfillmentTransaction({
-			fromChainId: sourceChainId,
-			senderAddress: userSourceChainWallet.address,
-			fromToken: tokenAddress,
-			link,
-			squidRouterUrl: getSquidRouterUrl(true, false),
-			provider: sourceChainProvider,
-			fromTokenDecimals: 6,
-			tokenType: EPeanutLinkType.erc20,
-			apiUrl,
-			APIKey,
-		})
-		console.log('Computed x chain unsigned fulfillment transactions', xchainUnsignedTxs)
-
-		for (const unsignedTx of xchainUnsignedTxs.unsignedTxs) {
-			const { tx, txHash } = await signAndSubmitTx({
-				unsignedTx,
-				structSigner: {
-					signer: userSourceChainWallet,
-					gasLimit: BigNumber.from(2_000_000),
-				},
+			const { link } = await peanut.createRequestLink({
+				chainId: destinationToken.chain,
+				tokenAddress: destinationToken.address,
+				tokenAmount: amount,
+				tokenType: destinationToken.type,
+				tokenDecimals: destinationToken.decimals.toString(),
+				recipientAddress,
+				APIKey,
+				apiUrl,
 			})
+			console.log('Created a request link on the source chain!', link)
 
-			console.log('Submitted a transaction to fulfill the request link with tx hash', txHash)
-			await tx.wait()
-			console.log('Request link fulfillment initiated!')
-		}
+			const linkDetails = await peanut.getRequestLinkDetails({
+				link,
+				APIKey,
+				apiUrl,
+			})
+			console.log('Got the link details!', linkDetails)
 
+			const xchainUnsignedTxs = await peanut.prepareXchainRequestFulfillmentTransaction({
+				fromChainId: sourceToken.chain,
+				senderAddress: userSourceChainWallet.address,
+				fromToken: sourceToken.address,
+				link,
+				squidRouterUrl: getSquidRouterUrl(true, false),
+				provider: sourceChainProvider,
+				tokenType: sourceToken.type,
+				fromTokenDecimals: sourceToken.decimals,
+				apiUrl,
+				APIKey,
+			})
+			console.log('Computed x chain unsigned fulfillment transactions', xchainUnsignedTxs)
+
+			for (const unsignedTx of xchainUnsignedTxs.unsignedTxs) {
+				const { tx, txHash } = await signAndSubmitTx({
+					unsignedTx,
+					structSigner: {
+						signer: userSourceChainWallet,
+						gasLimit: BigNumber.from(2_000_000),
+					},
+				})
+
+				console.log('Submitted a transaction to fulfill the request link with tx hash', txHash)
+				await tx.wait()
+				console.log('Request link fulfillment initiated!')
+			}
+			// TODO: expect something here
+			/*
 		const finalBalance = await peanut.getTokenBalance({
 			tokenAddress: destinationToken,
 			walletAddress: recipientAddress,
@@ -80,141 +130,8 @@ describe('Peanut XChain request links fulfillment tests', function () {
 		})
 		console.log('Final balance of recipient:', finalBalance)
 		// expect(finalBalance).toBe((Number(initialBalance) + amountToTestWith).toString())
-	}, 120000)
-
-	it('ETH on Optimism to ETH on Arbitrum', async function () {
-		peanut.toggleVerbose(true)
-		const userPrivateKey = process.env.TEST_WALLET_X_CHAIN_USER!
-
-		// Parameters that affect the test behaviour
-		const sourceChainId = '10' // Optimism
-		const destinationChainId = '42161' // Arbitrum
-		const amountToTestWith = 0.0001
-		const tokenDecimals = '18'
-		const apiUrl = process.env.PEANUT_API_URL!
-		const APIKey = process.env.PEANUT_DEV_API_KEY!
-		const sourceChainProvider = await getDefaultProvider(sourceChainId)
-		console.log('Source chain provider', sourceChainProvider)
-
-		const userSourceChainWallet = new ethers.Wallet(userPrivateKey, sourceChainProvider)
-
-		const recipientAddress = new ethers.Wallet(process.env.TEST_WALLET_PRIVATE_KEY2!).address
-		const tokenAddress = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' // ETH on Optimism
-		const destinationToken = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' // ETH on Arbitrum
-
-		const { link } = await peanut.createRequestLink({
-			chainId: destinationChainId,
-			tokenAddress: destinationToken,
-			tokenAmount: amountToTestWith.toString(),
-			tokenType: EPeanutLinkType.native,
-			tokenDecimals,
-			recipientAddress,
-			APIKey,
-			apiUrl,
-		})
-		console.log('Created a request link on the source chain!', link)
-
-		const linkDetails = await peanut.getRequestLinkDetails({
-			link,
-			APIKey,
-			apiUrl,
-		})
-		console.log('Got the link details!', linkDetails)
-
-		const xchainUnsignedTxs = await peanut.prepareXchainRequestFulfillmentTransaction({
-			fromChainId: sourceChainId,
-			senderAddress: userSourceChainWallet.address,
-			fromToken: tokenAddress,
-			link,
-			squidRouterUrl: getSquidRouterUrl(true, false),
-			provider: sourceChainProvider,
-			tokenType: EPeanutLinkType.native,
-			fromTokenDecimals: 18,
-			apiUrl,
-			APIKey,
-		})
-		console.log('Computed x chain unsigned fulfillment transactions', xchainUnsignedTxs)
-
-		for (const unsignedTx of xchainUnsignedTxs.unsignedTxs) {
-			const { tx, txHash } = await signAndSubmitTx({
-				unsignedTx,
-				structSigner: {
-					signer: userSourceChainWallet,
-					gasLimit: BigNumber.from(2_000_000),
-				},
-			})
-
-			console.log('Submitted a transaction to fulfill the request link with tx hash', txHash)
-			await tx.wait()
-			console.log('Request link fulfillment initiated!')
-		}
-	}, 120000)
-
-	it('ETH on Optimism to USDC on Optimism', async function () {
-		peanut.toggleVerbose(true)
-		const userPrivateKey = process.env.TEST_WALLET_X_CHAIN_USER!
-
-		// Parameters that affect the test behaviour
-		const sourceChainId = '10' // Arbitrum
-		const destinationChainId = '10' // Optimism
-		const amountToTestWith = 0.1
-		// const tokenDecimals = '18'
-		const apiUrl = process.env.PEANUT_API_URL!
-		const APIKey = process.env.PEANUT_DEV_API_KEY!
-		const sourceChainProvider = await getDefaultProvider(sourceChainId)
-		console.log('Source chain provider', sourceChainProvider)
-
-		const userSourceChainWallet = new ethers.Wallet(userPrivateKey, sourceChainProvider)
-
-		const recipientAddress = new ethers.Wallet(process.env.TEST_WALLET_PRIVATE_KEY2!).address
-		const tokenAddress = '0x0000000000000000000000000000000000000000' // ETH on Optimism
-		const destinationToken = '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85' // USDC on Optimism
-
-		const { link } = await peanut.createRequestLink({
-			chainId: destinationChainId,
-			tokenAddress: destinationToken,
-			tokenAmount: amountToTestWith.toString(),
-			tokenType: EPeanutLinkType.erc20,
-			tokenDecimals: '6',
-			recipientAddress,
-			APIKey,
-			apiUrl,
-		})
-		console.log('Created a request link on the source chain!', link)
-
-		const linkDetails = await peanut.getRequestLinkDetails({
-			link,
-			APIKey,
-			apiUrl,
-		})
-		console.log('Got the link details!', linkDetails)
-
-		const xchainUnsignedTxs = await peanut.prepareXchainRequestFulfillmentTransaction({
-			fromChainId: sourceChainId,
-			senderAddress: userSourceChainWallet.address,
-			fromToken: tokenAddress,
-			link,
-			squidRouterUrl: getSquidRouterUrl(true, false),
-			provider: sourceChainProvider,
-			tokenType: EPeanutLinkType.native,
-			fromTokenDecimals: 18,
-			apiUrl,
-			APIKey,
-		})
-		console.log('Computed x chain unsigned fulfillment transactions', xchainUnsignedTxs)
-
-		for (const unsignedTx of xchainUnsignedTxs.unsignedTxs) {
-			const { tx, txHash } = await signAndSubmitTx({
-				unsignedTx,
-				structSigner: {
-					signer: userSourceChainWallet,
-					gasLimit: BigNumber.from(2_000_000),
-				},
-			})
-
-			console.log('Submitted a transaction to fulfill the request link with tx hash', txHash)
-			await tx.wait()
-			console.log('Request link fulfillment initiated!')
-		}
-	}, 120000)
+    */
+		},
+		120000
+	)
 })
